4021a63574262a8b8d3a44e42534c9b8b3ba1b77
zimrancis <zimrancis@gmail.com>
2023-09-17 06:00:52 +0600

    Delete src directory

--- src/main/java/com/userfront/dao/SavingsAccountDao.java
+++
@@ -1,9 +0,0 @@
-package com.userfront.dao;
-
-import com.userfront.domain.SavingsAccount;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SavingsAccountDao extends CrudRepository<SavingsAccount, Long> {
-
-    SavingsAccount findByAccountNumber (int accountNumber);
-}
